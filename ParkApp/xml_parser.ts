f42639d82a23ed8fe4f40c258a42276f43cbb1dd
System Administrator <root@dhcp-128-189-238-191.ubcsecure.wireless.ubc.ca>
2015-11-03 15:52:42 -0800

    Added xml2json.ts library and modified xml_parser

--- ParkApp/xml_parser.ts
+++ ParkApp/xml_parser.ts
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="./Scripts/xml2json/xml2json.ts"/>
+
+module xml_parser {
+	export class xmlParser {
+
+		parse(data) {
+			var jsonData = xml2json.XMLParser.parse(data);
+			var ret = ret.COVParksFacilities;
+			return ret;
+		}
+	}
+}
+
