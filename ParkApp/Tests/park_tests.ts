8785d83253065f6cc785f6f8703989e58e715e1d
avetismu <avetismu@gmail.com>
2015-11-03 13:19:05 -0800

    added SearchBox for Map and modified style sheet

--- ParkApp/Tests/park_tests.ts
+++ ParkApp/Tests/park_tests.ts
@@ -1,29 +1,37 @@
 ﻿/* Testing Module for Parks*/
 /// <reference path="../park.ts"/>
 /// <reference path="../Scripts/tsUnit/tsUnit.ts"/>
+
+
 module park_test {
 
     export class ParkTests extends tsUnit.TestClass {
     
         //Instantiate Tests
-        testInstance() {
+        //testInstance() {
 
-            var park01 = new park.Park(
-                "Unknown Park",
-                12.0,
-                11.0,
-                true
-            );
+        //    var park01 = new park.Park(
+        //        "Unknown Park",
+        //        12.0,
+        //        11.0,
+        //        true
+        //    );
 
-            //Test name
-            this.areIdentical("Unknown Park", park01.name);
+        //    //Test name
+        //    this.areIdentical("Unknown Park", park01.name);
 
-            //Test latlong
-            //var latLong01 = new google.maps.LatLng(111.0, 1111.0, false);
-            //this.areIdentical(latLong01, park01.latlong);
+        //    //Test latlong
+        //    //var latLong01 = new google.maps.LatLng(111.0, 1111.0, false);
+        //    //this.areIdentical(latLong01, park01.latlong);
 
-            //Test facilities
-            this.areIdentical(true, park01.hasFacilities);
-        }
+        //    //Test facilities
+        //    this.areIdentical(true, park01.hasFacilities);
+        //}
     }
+}
+
+window.onload = () => {
+    /* Mock Application */
+    var el = document.getElementById('park_tests');
+
 }
--- ParkApp/app.js
+++ ParkApp/app.js
@@ -19,6 +19,22 @@ var Map = (function () {
         };
         var marker = new google.maps.Marker(markerOptions);
     };
+    Map.prototype.setInput = function (input) {
+        this.map.controls[google.maps.ControlPosition.LEFT_TOP].push(input);
+    };
+    Map.prototype.addLocationMarker = function (latlong) {
+        if (this.locationMarker != null) {
+            this.locationMarker.setMap(null);
+            this.locationMarker.setVisible(false);
+        }
+        //create marker to be added
+        var markerOptions = {
+            position: latlong,
+            icon: "https://maps.google.com/mapfiles/kml/shapes/horsebackriding.png",
+            map: this.map //adds marker to map
+        };
+        this.locationMarker = new google.maps.Marker(markerOptions);
+    };
     return Map;
 })();
 var Greeter = (function () {
@@ -52,6 +68,20 @@ window.onload = function () {
     var parkToBeMarked02 = new park.Park("untitled", 49.2500, -123.2100, false);
     googleMap.addParkMarker(parkToBeMarked01);
     googleMap.addParkMarker(parkToBeMarked02);
+    //Add Search Box
+    var elSearchBox = document.getElementById("search_box");
+    //var searchBoxOptions = googleMap.getMapBounds();
+    var searchBox = new google.maps.places.SearchBox(elSearchBox);
+    //attach SearchBox to Map
+    googleMap.setInput(elSearchBox);
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+        if (places.length == 0)
+            return;
+        places.forEach(function (place) {
+            googleMap.addLocationMarker(place.geometry.location);
+        });
+    });
     /* Park Instantiation Test*/
     var el2 = document.getElementById('park_tests');
     //instance of park
