255a9b416b6e192414949386a2bd55dc6e434ad9
avetismu <avetismu@gmail.com>
2015-11-03 13:33:31 -0800

    soft commit

--- ParkApp/app.ts
+++ ParkApp/app.ts
@@ -4,8 +4,9 @@
 class Map {
 
     public name: string;
-    private map: any;
+    private map: google.maps.Map;
     private options: any;
+    private locationMarker: google.maps.Marker;
 
     constructor(mapDiv: Element) {
         this.name = "GoogleMap";
@@ -25,6 +26,26 @@ class Map {
             map: this.map //adds marker to map
         };
         var marker = new google.maps.Marker(markerOptions);
+    }
+
+    setInput(input: HTMLInputElement) {
+        this.map.controls[google.maps.ControlPosition.LEFT_TOP].push(input);
+    }
+
+    addLocationMarker(latlong: google.maps.LatLng) {
+
+        if (this.locationMarker != null) {
+            this.locationMarker.setMap(null);
+            this.locationMarker.setVisible(false);
+        }
+
+        //create marker to be added
+        var markerOptions = {
+            position: latlong,
+            icon: "https://maps.google.com/mapfiles/kml/shapes/horsebackriding.png",
+            map: this.map //adds marker to map
+        };
+        this.locationMarker = new google.maps.Marker(markerOptions);
     }
 
 }
@@ -72,6 +93,34 @@ window.onload = () => {
     var parkToBeMarked02 = new park.Park("untitled", 49.2500, -123.2100, false);
     googleMap.addParkMarker(parkToBeMarked01);
     googleMap.addParkMarker(parkToBeMarked02);
+
+    //Add Search Box
+    var elSearchBox = <HTMLInputElement>document.getElementById("search_box");
+
+    //var searchBoxOptions = googleMap.getMapBounds();
+    var searchBox = new google.maps.places.SearchBox(
+        elSearchBox
+        //searchBoxOptions
+    );
+
+    //attach SearchBox to Map
+    googleMap.setInput(elSearchBox);
+
+    searchBox.addListener(
+        'places_changed',
+        () => {
+            var places = searchBox.getPlaces()
+
+            if (places.length == 0)
+                return;
+            
+            places.forEach((place) => {
+                googleMap.addLocationMarker(place.geometry.location);
+            });
+        }
+    );
+
+    
 
 
 
