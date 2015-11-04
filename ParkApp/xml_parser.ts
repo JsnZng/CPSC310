13e1d75eb2ec39a46541bac3782bb23ae678b77e
System Administrator <root@dhcp-206-87-124-62.ubcsecure.wireless.ubc.ca>
2015-11-04 08:21:17 -0800

    Added xmlParser.get() and ftp library

--- ParkApp/xml_parser.ts
+++ ParkApp/xml_parser.ts
@@ -1,13 +1,44 @@
 ﻿/// <reference path="./Scripts/xml2json/xml2json.ts"/>
+/// <reference path ="./Scripts/ftp/ftp.d.ts"/>
+
+import ftp = require('ftp');
 
 module xml_parser {
 	export class xmlParser {
-		
-		public static parse(data) {
+
+		store(data) {
+
+		}
+
+		parse(data) {
 			var jsonData = xml2json.XMLParser.parse(data);
 			var ret = ret.COVParksFacilities;
 			return ret;
 		}
+
+		public static get() {
+			var c = new ftp();
+			var hostName = 'webftp.vancouver.ca';
+			var path =  '/opendata/xml/parks_facilities.xml;';
+			var options = {host : hostName};
+			var data;
+			c.on('ready', function() {
+				c.get(path, function(err, stream) {
+					if (err) throw err;
+					stream.on('data',function(d) {
+						data += d.toString;
+					});
+					stream.on('end', function() {
+						//parse and store the data we just got
+						var parsedData = this.parse(data);
+						this.store(data);
+					})
+				});
+			});
+			c.connect(options);
+
+		}
+
 	}
 }
 
