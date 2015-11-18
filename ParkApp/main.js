e619382fb1e9a9cb0c9af9e4c0346bec76185101
GaohanC <gaohanc@alumni.ubc.ca>
2015-11-18 11:14:48 -0800

    Update main.js

--- ParkApp/main.js
+++ ParkApp/main.js
@@ -2,18 +2,18 @@ function clicked(){
 	var un = document.getElementById('username');
 	var pw = document.getElementById('password');
 
-	var corun = "test";
+	var corun = "abc";
 	var corpw = "123";
 
 	if(un.value == corun){
 		if(pw.value == corpw){
-		window.alert("Logged in as" + un.value);
-		
+		window.alert("Logged in as " +  un.value);
+
 		}else{
 			window.alert("Invalid username or password!")
-		}else{
-		window.alert("Invalid username or password!")
-		}
+		    }
+	     } else {
+	        window.alert("Invalid username or password!")
 	}
 
 }
